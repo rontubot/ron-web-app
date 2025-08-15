5cf85459d2ced51a9fa8b515936d3daa5c0bc642
rontubot <rontubot@gmail.com>
2025-08-15 23:43:10 +0000

    act app web

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-export default App;
-export default App;
-export default App;
 import React from 'react';  
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';  
 import { AuthProvider, useAuth } from './context/AuthContext';  

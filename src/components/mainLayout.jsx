b8ec6313f3a9cd47b32a68d5b5393322b6c17a97
rontubot <rontubot@gmail.com>
2025-09-03 18:58:16 -0300

    electron

--- src/components/mainLayout.jsx
+++ src/components/mainLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';  
 import { useLocation, useNavigate } from 'react-router-dom';  
 import { useAuth } from '../context/authcontext';  
-import './MainLayout.css';  
+import './mainLayout.css';  
   
 const MainLayout = ({ children }) => {  
   const location = useLocation();  
